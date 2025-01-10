bb739b1af31e508179b11e52a5510e8593622257
Green <rusman_ml@mail.ru>
2025-01-10 22:42:31 +0700

    Add files via upload

---
+++ Sigment2.py
@@ -0,0 +1,15 @@
+# "1st program"
+print(9**0.5*5)
+
+#"2nd program"
+print(9.99>9.98 or 1000!=1000.1)
+
+#"3nd program"
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2 == 2*(2+2))
+
+#"4th program"
+print(str("123.456"))
+print(type(int(123.456)))
+print(int(123.456*10))
